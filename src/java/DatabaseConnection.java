82ad9d38a135dc8a49eb08393a1253b23c78942f
imranjk02 <imrankotwal02@gmail.com>
2026-09-16 23:25:34 +0530

    Connect to Aiven database

--- src/java/DatabaseConnection.java
+++ src/java/DatabaseConnection.java
@@ -3,10 +3,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+
     public static Connection getConnection() throws SQLException {
-        String url = System.getenv("DB_URL");
+        String host = System.getenv("DB_HOST");
+        String port = System.getenv("DB_PORT");
+        String database = System.getenv("DB_NAME");
         String user = System.getenv("DB_USER");
         String password = System.getenv("DB_PASSWORD");
+
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?sslMode=REQUIRED";
 
         return DriverManager.getConnection(url, user, password);
     }
